d8cd7524979ec3f3f91dfbafde5b3e8c790fc15f
AvilaJoseph <avilajoseph2021@gmail.com>
2025-03-10 23:12:39 -0500

    estructuración del proyecto sonora

---
+++ src/app/sonora/pages/SonoraHome/SonoraHome.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sonora-home',
+  imports: [],
+  templateUrl: './SonoraHome.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SonoraHomeComponent { }
