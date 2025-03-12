db36061dbf93fc3e95ad0f4e0e47a2348b619c4d
AvilaJoseph <avilajoseph2021@gmail.com>
2025-03-11 23:46:31 -0500

    Creaciones de pages

---
+++ src/app/sonora/pages/Courses/Courses.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  imports: [],
+  templateUrl: './Courses.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CoursesComponent { }
