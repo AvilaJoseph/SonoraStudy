790c785c66d30482952a566f73a070ebb88d3a55
AvilaJoseph <avilajoseph2021@gmail.com>
2025-03-11 23:11:02 -0500

    creación de componentes en el orden correcto

---
+++ src/app/shared/components/Footer/Footer.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './Footer.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent { }
--- src/app/shared/pages/SonoraHome/SonoraHome.component.ts
+++ src/app/shared/pages/SonoraHome/SonoraHome.component.ts
@@ -1,8 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeroComponent } from '../../components/Hero/Hero.component';
+import { FooterComponent } from '../../components/Footer/Footer.component';
 
 @Component({
   selector: 'app-sonora-home',
-  imports: [],
+  imports: [
+    HeroComponent,
+    FooterComponent,
+  ],
   templateUrl: './SonoraHome.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
